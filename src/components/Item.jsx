b4512c2d945ae7f101d3de4133c94b76760ea615
hw7319 <hw7319@naver.com>
2023-03-20 17:44:29 +0900

    video detail ui 작업중...

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -7,24 +7,30 @@ export default function Item({ video, title, img }) {
   return (
     <div
       className={
-        keyword != null
-          ? "flex  xl:w-1/3 h-72 mr-2 mb-2 w-72"
-          : "flex flex-col h-72 mb-2 w-2/3 xl:w-96"
+        keyword
+          ? "flex  h-72 mb-2 w-2/3 xl:w-full"
+          : "flex flex-col h-72  mb-2 w-2/3 xl:w-96"
       }
     >
-      <div className="flex w-full h-52 bg-slate-300 rounded-md">
-        {/* <img src={img} alt="" className="rounded-md w-full" /> */}
+      <div
+        className={
+          keyword
+            ? "flex w-1/3 h-52 bg-slate-300 rounded-md"
+            : "flex w-full h-52 bg-slate-300 rounded-md"
+        }
+      >
+        <img src={img} alt="" className="rounded-md w-full" />
       </div>
       <div className="p-2">
         <div className="flex w-8 h-8 bg-red-400 rounded-full shrink-0">
-          {/* <img
+          <img
             src="http://news.samsungdisplay.com/wp-content/uploads/2018/08/8.jpg"
             alt=""
             className="w-8 h-8 rounded-full"
-          /> */}
+          />
         </div>
         <div clas="flex flex-col">
-          <div className="pl-2 font-mediun font-sans">{title}</div>
+          <div className="pl-2 font-mediun font-sans h-auto">{title}</div>
           <div className="pl-2 text-sm text-slate-500 font-sans">YTN</div>
           <div className="pl-2 flex text-sm text-slate-500">
             <div>조회수 107만회</div>
