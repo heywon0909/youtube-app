0a6585e6873d193cc72c715f1ebd532856de6ce6
hw7319 <hw7319@naver.com>
2023-03-23 14:33:31 +0900

    home.jsx react query로 변경

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,38 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Item from "../components/Item";
+import { useQuery } from "@tanstack/react-query";
 export default function Home() {
-  const [videos, setVideos] = useState([]);
-  useEffect(() => {
-    // console.log('몇번 호출되는거얌')
-    fetch(`data/list.json`)
+  const getVideos = async () => {
+    return await (
+      await fetch(`http://localhost:3000/data/list.json`)
+    )
       .then((res) => res.json())
       .then((data) => {
         const { items } = data;
-        //   console.log('items',items)
-        setVideos(items);
-        //   console.log('videos',videos)
-      });
-  }, [videos]);
+        return items;
+      })
+      .catch((error) => console.log("error", error));
+  };
+  const { isLoading, data: items } = useQuery(["video_list"], getVideos);
+  console.log("data", items);
+
   return (
     // <div className="block w-full pt-5">
     <div className="flex justify-center items-center h-screen">
       <div className="flex flex-wrap xl:justify-between justify-center md:justify-center xl:w-11/12 h-full mt-32">
-        {videos.map((video) => {
-          return (
-            <Item
-              video={video}
-              title={video.snippet.title}
-              img={video.snippet.thumbnails.medium.url}
-              key={video.etag}
-              id={video.id.videoId}
-            />
-          );
-        })}
+        {!isLoading &&
+          items.map((video) => {
+            return (
+              <Item
+                video={video}
+                title={video.snippet.title}
+                img={video.snippet.thumbnails.medium.url}
+                key={video.etag}
+                id={video.id.videoId}
+              />
+            );
+          })}
+        {isLoading && <p>isLoading...</p>}
       </div>
     </div>
     // </div>
