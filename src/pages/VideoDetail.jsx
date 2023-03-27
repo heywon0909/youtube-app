7d3a1dfbaf905d40584e49403839122c1dfa56b0
hw7319 <hw7319@naver.com>
2023-03-27 09:11:04 +0900

    video isLoading 삭제 ->html tag 삭제

--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -48,7 +48,6 @@ export default function VideoDetail() {
                 />
               );
             })}
-          {isLoading && <p>isLoading...</p>}
         </div>
       </div>
     </div>
